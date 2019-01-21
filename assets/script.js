c1c04823d3f5bf128f3d253f396f817b1c44557c
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-01-21 14:39:00 -0600

    Ready to submit!
    Could go in like this

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Array for buttons which will be passed in as values
-var buttons = ["Cookies", "Pies", "Pastries"];
-var clickCount = 0;
+var buttons = ["Cookies", "Pies", "Pastries", "Jelly Beans",
+    "Ice Cream Sandwich", "Donut"];
+var click = 0;
 
 
 makeButton();
@@ -21,8 +22,14 @@ function makeButton() {
     });
 };
 
+$("btn").on("click", function () {
+    $(".giph-section").empty();
+});
+
 // Click event listener
 $("button").on("click", function () {
+    $(".giph-section").empty();
+
     var giphySearch = $(this).attr("searchTerm")
     var giphySearchURL = "http://api.giphy.com/v1/gifs/search?q=" +
         giphySearch + "&api_key=GEpLlvx43Yy5aSTYLfGZcsH4pkPInlxp&limit=10";
@@ -33,20 +40,40 @@ $("button").on("click", function () {
     })
         .then(function (response) {
             var ajaxResponse = response.data;
-            console.log(ajaxResponse);
             for (var i = 0; i < ajaxResponse.length; i++) {
                 // Make a new div
                 var newGiph = $("<div>");
+                newGiph.attr("id", "imageDiv")
                 // and make a new image
                 var buttonImage = $("<img>");
                 // give the image source the STILL url
                 buttonImage.attr("src", ajaxResponse[i].images.fixed_height_still.url);
+                // Still URL to facilitate animation
+                buttonImage.attr("giphStill", ajaxResponse[i].images.fixed_height_still.url);
+                // Moving URL to facilitate animation
+                buttonImage.attr("giphPlay", ajaxResponse[i].images.fixed_height.url);
+                // State so  that animated/still can be tracked
+                buttonImage.attr("giphState", "still");
                 // add image to newGiph div
-                newGiph.append(buttonImage);
+                newGiph.prepend(buttonImage);
                 // and write newGiph div to the DOM
                 $(".giph-section").append(newGiph);
             }
         });
 })
 
-    // MAYBE text box to add giphs
+$(document).on("click", "img", function () {
+    click++;
+    var state = $(this).attr("giphState");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("giphPlay"));
+        $(this).attr("giphState", "play");
+    }
+    else {
+        $(this).attr("src", $(this).attr("giphStill"));
+        $(this).attr("giphState", "still");
+    }
+
+
+});
